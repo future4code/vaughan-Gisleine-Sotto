797c29702e52ed7f6dfb803ee837f312bc879270
Gisleine Rosario da Silva Sotto <gi@sotto.com.br>
2021-12-02 22:54:20 -0300

    Funções 02/12/21

--- modulo1/funcoes/index.js
+++ modulo1/funcoes/index.js
@@ -119,55 +119,82 @@
             console.log(soma(5,3) )  //retorna 8
 
 
-
-
-
-
-            /*b) Faça uma função que recebe 2 números e retorne um
+           /*b) Faça uma função que recebe 2 números e retorne um
              booleano que 
             informa se o primeiro número é **maior ou igual** ao
-             segundo.
+             segundo.*/
             
+             function maiorIgual(a, b) {
+                return a >= b 
+
+             }
+                     
+            console.log(maiorIgual(8,9));
 
 
-
-
-            c) Escreva uma função que receba um número e devolva um 
+           /* c) Escreva uma função que receba um número e devolva um 
             ]booleano
-             indicando se ele é par ou não
+             indicando se ele é par ou não*/
             
+             function numeroPar(a){
+
+                return a % 2 == 0
+             }
+              console.log(numeroPar(4));
 
 
 
-
-
-
-
-            d) Faça uma função que recebe uma mensagem (`string`) 
+            /*d) Faça uma função que recebe uma mensagem (`string`) 
             como parâmetro
              e imprima o tamanho dessa mensagem, juntamente com uma
               versão dela em 
-             letras maiúsculas.
+             letras maiúsculas.*/
             
+             function tamanhoMaiuscula(frase){
+
+                let contagem = frase.length
+                console.log(contagem)
+
+                let maiusculas = frase.toUpperCase()
+                console.log(maiusculas);
+
+             }
 
 
 
 
-
-        3. Crie uma função para cada uma das operações básicas
+        /*3. Crie uma função para cada uma das operações básicas
          (soma, subtração, 
             multiplicação e divisão). Em seguida, peça para o usuário
              inserir dois números e **chame** essas 4 funções com esses
               valores inputados pelo usuário sendo o argumento. Por fim, mostre
-               no console o resultado das operações:
+               no console o resultado das operações:*/
             
+             function soma(a,b){
+                return a+b;
+             }
+                 function subtracao(a,b){
+                 return a-b;    
+                 }
 
+                 function  multiplicacao(a,b) {
+                     return a*b;
+                 }
+             
+                function divisao(a,b) {
+                    return a/b;
+    
+                    }
 
+                    let valor1 = Number(prompt("Digite um numero"));
+                    let valor2 = Number (prompt("Digite outro numero"));
+                    
+                    console.log("soma", soma (valor1, valor2));
+                    console.log("subtracao", subtracao (valor1, valor2));
+                    console.log("multiplicacao", multiplicacao (valor1, valor2));
+                    console.log("divisao", divisao (valor1, valor2));
 
-
-
-            
-            Números inseridos: 30 e 3
+            /*Números inseridos: 30 e 3
             Soma: 33
             Diferença: 27
             Multiplicação: 90
